fc64f111b7ecabb05d40fa7ba37eb3e026db8656
Iván Tortosa <152330066+LiquidRay-1@users.noreply.github.com>
2024-05-08 19:23:56 +0200

    Add files via upload

---
+++ clase3AR/clase3AR/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  clase3AR
+//
+//  Created by dam2 on 8/5/24.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func OnButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func OffButton(_ sender: Any) {
+        
+    }
+    
+}
+
